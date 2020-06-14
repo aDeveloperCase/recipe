588f74f4cc6c18b88c81925d90d1846735ec7d46
aDeveloperCase <rroberto@posteo.net>
2020-06-14 17:42:25 +0200

    upload app

---
+++ precache-manifest.d45dfece9d2ccaabd2c42cddb3e8c915.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a4ee4a9ea58a6e8ee91751d5edac969",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dffd58ed46da7415bee7",
+    "url": "/static/css/main.84ab2bfc.chunk.css"
+  },
+  {
+    "revision": "e0a6ce2a23fd5b36796e",
+    "url": "/static/js/2.6a7284ca.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/static/js/2.6a7284ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dffd58ed46da7415bee7",
+    "url": "/static/js/main.eea3e179.chunk.js"
+  },
+  {
+    "revision": "feeb7418a0faacc99f87",
+    "url": "/static/js/runtime-main.c939cca0.js"
+  }
+]);
